cd1fb1bcbf796044b045ff3f6f25921fd70c40c6
Web-dev-experts <itworksonmymachinedev@gmail.com>
2026-01-19 06:23:14 +0100

    FIX server config for deployment & REMOVE console logs

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ require('dotenv').config({ path: './config.env' });
 const app = require('./app.js');
 
 const PORT = process.env.PORT || 3000;
-const HOSTNAME = 'localhost';
 
-app.listen(PORT, HOSTNAME, () => {
+app.listen(PORT, () => {
   console.log(`Server listening on http://${HOSTNAME}:${PORT}`);
 });
