d15097308f4619dc73ca390af135aefb0aa4b569
Web-dev-experts <itworksonmymachinedev@gmail.com>
2026-01-19 06:19:53 +0100

    Fix server config for deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config({ path: './config.env' });
 const app = require('./app.js');
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const HOSTNAME = 'localhost';
 
 app.listen(PORT, HOSTNAME, () => {
